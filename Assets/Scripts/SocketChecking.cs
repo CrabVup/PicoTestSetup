a5469ad340fd7f4a77ab520a01be01439e505951
Floorban <15696082812@163.com>
2023-09-23 02:59:59 +0200

    different turrets

--- Assets/Scripts/SocketChecking.cs
+++ Assets/Scripts/SocketChecking.cs
@@ -6,19 +6,26 @@ public class SocketChecking : MonoBehaviour
 {
 
     XRSocketInteractor socket;
-
+    public List<GameObject> Turrets;
 
     void Start()
     {
         socket = GetComponent<XRSocketInteractor>();
+        foreach (GameObject turret in Turrets)
+        {
+            Tower tower = turret.GetComponent<Tower>();
+            if (tower != null)
+            {
+                // You have found the Tower script on the GameObject 'turret'
+                // You can now work with 'tower'
+            }
+        }
     }
 
     public void socketCheck()
     {
 
         IXRSelectInteractable objName = socket.GetOldestInteractableSelected();
-
-
         Debug.Log(objName.transform.name + " in socket of " + transform.name);
 
     }
@@ -27,9 +34,23 @@ public class SocketChecking : MonoBehaviour
     {
         
         IXRSelectInteractable obj = socket.GetOldestInteractableSelected();
-        if(obj.transform.tag == "interactable")
+
+        if(obj.transform.tag == "TurretA")
         {
-            Debug.Log("wtf");
+            
+            Debug.Log("A");
+        }
+
+        if (obj.transform.tag == "TurretB")
+        {
+           
+            Debug.Log("B");
+        }
+
+        if (obj.transform.tag == "TurretC")
+        {
+          
+            Debug.Log("C");
         }
 
 
