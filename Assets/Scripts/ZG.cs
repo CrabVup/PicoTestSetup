3bcc34dded8f60dba3f2667fe3e2cdb04f6d0ad2
Butterfly-Nebula <mihaelabuzatu173@gmail.com>
2023-10-18 22:25:55 +0200

    NewScene with Hexabody
    NOT COMPLETED

--- Assets/Scripts/ZG.cs
+++ Assets/Scripts/ZG.cs
@@ -1,17 +1,33 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEditor.XR.LegacyInputHelpers;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
 
 public class ZG : MonoBehaviour
 {
+    [Header("Movement")]
     public float downAndUpSpeed;
     public float speed;
     public float maxSpeed = 1.0f; // Maximum speed in units per second.
+
+    [Header("XR Tolkit Parts")]
+    public ARSessionOrigin xrOrigin;
     public GameObject mainCamera; // Reference to the main camera.
+
+    [Header("Hexabody Parts")]
+    public GameObject Head;
+    public GameObject Chest;
+    public GameObject Fender;
+    public GameObject Monoball;
 
     private Rigidbody rb;
 
-    public Scan scan;
+    //public Scan scan;
+
+    private Quaternion headYaw;
 
     void Start()
     {
@@ -41,6 +57,9 @@ public class ZG : MonoBehaviour
             cameraForward.Normalize();
             cameraRight.Normalize();
 
+            // Get Controllers Input
+            headYaw = Quaternion.Euler(0, xrOrigin.cameraGameObject.transform.eulerAngles.y, 0); // set the head to the camera's rotation on the y axis
+
             // Calculate movement based on input axes
             Vector3 moveDirection = (cameraForward * Input.GetAxis("Vertical1") +
                                      cameraRight * Input.GetAxis("Horizontal1")).normalized;
@@ -58,5 +77,27 @@ public class ZG : MonoBehaviour
         {
             Debug.LogWarning("Main camera is not assigned!");
         }
+
+        // Hexab0dy
+
+        XRRigToPlayer();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rotatePlayer();
+    }
+
+    private void XRRigToPlayer()
+    {
+        XRRig.transform.position = new Vector3(Fender.transform.position.x, Fender.transform.position.y - (0.5f * Fender.transform.localScale.y + 0.5f * Monoball.transform.localScale.y), Fender.transform.position.z);
+
+    }
+
+    private void rotatePlayer()
+    {
+        Chest.transform.rotation = headYaw;
     }
 }
+*/
