65aed618f0587ff99d93377a48977924f2ecca4b
Butterfly-Nebula <mihaelabuzatu173@gmail.com>
2023-10-19 13:53:52 +0200

    ModifiedScripts

--- Assets/Scripts/Adina's/PhysicsBody.cs
+++ Assets/Scripts/Adina's/PhysicsBody.cs
@@ -1,43 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEditor.XR.LegacyInputHelpers;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
+using Unity.XR.CoreUtils;
 
-public class ZG : MonoBehaviour
+public class PhysicsBody : MonoBehaviour
 {
     [Header("Movement")]
     public float downAndUpSpeed;
     public float speed;
     public float maxSpeed = 1.0f; // Maximum speed in units per second.
 
-    public GameObject mainCamera;    // Reference to the main camera.
+    [Header("XR Tolkit Parts")]
+    public XROrigin xrOrigin;
+    public GameObject cameraObject;    // Reference to the main camera.
+
+    [Header("Hexabody Parts")]
+    public GameObject Head;
+    public GameObject Chest;
+    public GameObject Fender;
+    public GameObject Monoball;
 
     private Rigidbody rb;
 
-    public Scan scan;
+    private Quaternion headYaw;
+
+    private Camera cameraObject;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cameraObject = xrOrigin.GetComponent<XRManager>().camera;
     }
 
     void Update()
     {
-        if (Input.GetButton("Fire1"))
-        {
-            scan.GetInfo();
-            Debug.Log("Yes!");
-        }
-
         float upDownInput = Input.GetAxis("UpDown");
         Vector3 upDownForce = Vector3.up * upDownInput * downAndUpSpeed;
 
         rb.AddForce(upDownForce * Time.deltaTime);
 
         // Check if a camera is assigned
-        if (mainCamera != null)
+        if (cameraObject != null)
         {
-            Vector3 cameraForward = mainCamera.transform.forward;
-            Vector3 cameraRight = mainCamera.transform.right;
+            Vector3 cameraForward = cameraObject.transform.forward;
+            Vector3 cameraRight = cameraObject.transform.right;
 
             // Normalize the directions
             cameraForward.Normalize();
@@ -50,8 +60,8 @@ public class ZG : MonoBehaviour
             Vector3 moveDirection = (cameraForward * Input.GetAxis("Vertical1") +
                                      cameraRight * Input.GetAxis("Horizontal1")).normalized;
 
-        // Apply force in the calculated direction
-        rb.AddForce(moveDirection * speed * Time.deltaTime);
+            // Apply force in the calculated direction
+            rb.AddForce(moveDirection * speed * Time.deltaTime);
 
             // Limit the maximum speed
             if (rb.velocity.magnitude > maxSpeed)
@@ -63,5 +73,26 @@ public class ZG : MonoBehaviour
         {
             Debug.LogWarning("Main camera is not assigned!");
         }
+
+        // Hexab0dy
+
+        XRRigToPlayer();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rotatePlayer();
+    }
+
+    private void XRRigToPlayer()
+    {
+        XRRig.transform.position = new Vector3(Fender.transform.position.x, Fender.transform.position.y - (0.5f * Fender.transform.localScale.y + 0.5f * Monoball.transform.localScale.y), Fender.transform.position.z);
+
+    }
+
+    private void rotatePlayer()
+    {
+        Chest.transform.rotation = headYaw;
     }
 }
