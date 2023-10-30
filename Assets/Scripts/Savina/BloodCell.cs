1ae6296029158f375d02a260519034e76854577d
Floorban <15696082812@163.com>
2023-10-30 18:56:18 +0100

    blood cell modified

--- Assets/Scripts/Savina/BloodCell.cs
+++ Assets/Scripts/Savina/BloodCell.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BloodCell : MonoBehaviour
-{   
-    public float speed = 3;
-    public Vector3 target = new Vector3(0, 0, 1);
+{
+    public float minSpeed = 2f;
+    public float maxSpeed = 5f;
+    public float maxRotationSpeed = 100f;
+
+    private Vector3 initialRotation;
+    private Vector3 rotationAxis;
+    private float rotationSpeed;
+    private float speed;
+
+    private void Start()
+    {
+        // Randomly set the initial rotation
+        initialRotation = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
+        transform.eulerAngles = initialRotation;
+
+        // Randomly set the rotation axis
+        rotationAxis = Random.onUnitSphere;
+
+        // Randomly set the initial rotation speed
+        rotationSpeed = Random.Range(-maxRotationSpeed, maxRotationSpeed);
+
+        // Randomly set the cell speed
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
     private void Update()
     {
-        // movement of the cell from point A to B
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-        // rotation of the cell 
-        transform.Rotate(0.05f, 5f * Time.deltaTime, 0.05f, Space.Self);
+        // Move the cell forward along the global forward direction
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+
+        // Rotate the cell around the chosen rotation axis at a variable speed
+        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/Savina/CellSpawner.cs
+++ Assets/Scripts/Savina/CellSpawner.cs
@@ -7,13 +7,28 @@ public class CellSpawner : MonoBehaviour
     public GameObject spawnCell;
     public float frequency;
     public float initialSpeed;
+    public Transform plane; // Reference to the plane where you want to spawn cells
+    public Vector2 spawnAreaSize; // Define the size of the area on the plane where cells will spawn
+
     float lastSpawnTime;
+
     void Update()
     {
         if (Time.time > lastSpawnTime + frequency)
         {
-            // Spawn of the cells
-            Instantiate(spawnCell, transform.position, Quaternion.identity);
+            // Calculate random spawn position within the defined area
+            Vector3 randomSpawnPosition = new Vector3(
+                Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2),
+                0.0f,
+                Random.Range(-spawnAreaSize.y / 2, spawnAreaSize.y / 2)
+            );
+
+            // Convert local position to world position using the plane's transform
+            Vector3 worldSpawnPosition = plane.TransformPoint(randomSpawnPosition);
+
+            // Spawn the cells at the calculated position
+            Instantiate(spawnCell, worldSpawnPosition, Quaternion.identity);
+
             lastSpawnTime = Time.time;
         }
     }
