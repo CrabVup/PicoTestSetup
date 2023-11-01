cecd11bbed4f4c90c7e847e83d45a5a31530a6e0
CrabVup <67231352+CrabVup@users.noreply.github.com>
2023-11-01 22:58:21 +0100

    sound(not really)

---
+++ Assets/Sounds/SoundEffects.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+
+    public AudioSource scanSounds;
+    public AudioClip scanButtonPress, scanButtonRelease;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
