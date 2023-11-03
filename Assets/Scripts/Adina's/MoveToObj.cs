33bc719964bc71f1d17c98e5dcf799fd91020355
Butterfly-Nebula <mihaelabuzatu173@gmail.com>
2023-11-03 20:17:16 +0100

    T-Cell model

--- Assets/Scripts/Adina's/MoveToObj.cs
+++ Assets/Scripts/Adina's/MoveToObj.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class MoveToObj : MonoBehaviour
 {
     public Marker marker;
-    // The speed the virus is moving towards the marker
+    // The speed the T-Cells are moving towards the marker
     public float speed = 1.0f;
 
-    // The target (marker) position
+    // The target (virus) position
     public GameObject target;
 
     void Update()
     {
         if (marker.isPlaced)
         {
-       
-            // Move virus a step closer to the target
+            // Move T-Cell a step closer to the virus
             var step = speed * Time.deltaTime; // calculate distance to move
             transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
         
