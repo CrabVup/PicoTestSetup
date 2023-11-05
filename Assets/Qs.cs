3284ecc135fa02b88148e55e6c25e19ae9ae563c
Butterfly-Nebula <mihaelabuzatu173@gmail.com>
2023-11-05 14:59:06 +0100

    oneQuestIG

--- Assets/Qs.cs
+++ Assets/Qs.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Qs : MonoBehaviour
 {
     public Image image;
+    public TextMeshProUGUI scan_count;
 
     private void Awake()
     {
@@ -15,7 +17,7 @@ public class Qs : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       scan_count.text = "0/1";
     }
 
     // Update is called once per frame
@@ -24,6 +26,7 @@ public class Qs : MonoBehaviour
         if (GameObject.Find("XR Origin").GetComponent<ZG>().isScanned == true)
         {
             image.enabled = true;
+            scan_count.text = "1/1";
         }
     }
 }
