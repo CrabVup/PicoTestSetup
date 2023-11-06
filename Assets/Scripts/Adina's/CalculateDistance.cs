5da6df0c7d48f2bb7784e57a4546b781e5c6623c
Butterfly-Nebula <mihaelabuzatu173@gmail.com>
2023-11-06 18:57:43 +0100

    DistanceScriptFix

--- Assets/Scripts/Adina's/CalculateDistance.cs
+++ Assets/Scripts/Adina's/CalculateDistance.cs
@@ -53,6 +53,9 @@ public class CalculateDistance : MonoBehaviour
                 // distanceToString(F2) will show 2 digits after period (ex. 12.31)
                 distanceTextVirusA.text = "Distance Virus A: " + distance.ToString("F1") + "metres";
             }
+        } else
+        {
+            distanceTextVirusA.text = "Cleaned!";
         }
 
         if (virusBDestroyed == false)
@@ -63,7 +66,11 @@ public class CalculateDistance : MonoBehaviour
                 distanceTextVirusB.text = "Distance Virus B: " + distance.ToString("F1") + "metres";
             }
         }
-        
+        else
+        {
+            distanceTextVirusB.text = "Cleaned!";
+        }
+
         if (bacteriaDestroyed == false)
         {
             if (bacteria != null)
@@ -71,7 +78,11 @@ public class CalculateDistance : MonoBehaviour
                 distance = (bacteria.transform.position - transform.position).magnitude;
                 distanceTextBacteria.text = "Distance Bacteria: " + distance.ToString("F1") + "metres";
             }
-        } 
+        }
+        else
+        {
+            distanceTextBacteria.text = "Cleaned!";
+        }
     }
 
     private void FixedUpdate()
